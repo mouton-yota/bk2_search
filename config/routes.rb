0bec30ec82b0bdb96d19110fa14812bf927f9fa0
vagrant <vagrant@localhost.localdomain>
2020-08-23 03:45:37 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+
+  get 'relationships/create'
+  get 'relationships/destroy'
+  get 'favorites/create'
+  root 'home#top'
+  get 'home/about'
+
+
+  devise_for :users
+  resources :users, :only => [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get :follows, on: :member
+    get :followers, on: :member
+  end
+
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+
+end
